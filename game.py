fcfe84c3823e1731c8c14321ae35bc77313e3ea2
Konstantin Magin <konstantinmagin@yahoo.com>
2021-10-11 20:50:57 +0300

    game_file_changes_2

--- game.py
+++ game.py
@@ -2,34 +2,53 @@ import pygame
 import os
 from random import randint
 
+def get_var():
+    songs = get_songs()
+    if(songs == None):
+        return "ERROR!"
+    else:
+        print("Guess the song!\nVarients:\n" + str(songs))
+        num = input("Song? ")
+        return songs[int(num)]
+
 def get_songs():
     files = os.listdir("./")
-    songs = filter(lambda x: x.endswith('.mp3'), files)
+    songs = list(filter(lambda x: x.endswith('.mp3'), files))
     return songs
 
 def get_random_song():
     songs = get_songs()
-    random_number = randint(0, len(songs))
+    length = len(songs)
+    random_number = randint(0, length-1)
     return songs[random_number]
 
 def start_game():
     pygame.init()
-    song = pygame.mixer.Sound(get_random_song())
+    random_song = get_random_song()
+    song = pygame.mixer.Sound(random_song)
     clock = pygame.time.Clock()
     song.play()
     while True:
+        result = get_var()
         clock.tick(20)
-    pygame.quit()
+        if(result == random_song):
+            print("Success!")
+            pygame.quit()
+            return "Good"
+            
+        else:
+            print("Failure!")
+            pygame.quit()
+            return "Bad" 
 
 def menu(num):
-    print("input: ")
-    result = 0
+    print("You choosed: " + num)
+    start_game()
+    if(num == 1):
+        return 0
     if(num == 2):
-        result == 1
-    elif(num == 1):
-        start_game()
-        result == 0
-    return result
+        return 1
+
 
 print("Welcome to guess the melody game!\nChoose one of the options below!\n(1) - start the game, (2) - quit the game")
 menu(input())
